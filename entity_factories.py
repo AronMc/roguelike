3c0e8bd08e5eaea9f05b1bc22f7e3c8ac2f1abc8
Aron McGill <aronmcgill@gmail.com>
2023-05-20 10:09:13 +0100

    added scrolls and targeting

--- entity_factories.py
+++ entity_factories.py
@@ -1,13 +1,16 @@
 from components.ai import HostileEnemy
+from components import consumable
 from components.fighter import Fighter
-from entity import Actor
+from components.inventory import Inventory
+from entity import Actor, Item
 
 player = Actor(
     char ="@",
     color=(255, 255, 255),
     name="LabMouse",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=30, defense=2, power=5),
+    fighter=Fighter(hp=3, defense=2, power=5),
+    inventory=Inventory(capacity=2),
 )
 
 mouse = Actor(
@@ -15,12 +18,27 @@ mouse = Actor(
     color=(63, 127, 63),
     name="Mouse",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=10, defense=0, power=3),
+    fighter=Fighter(hp=1, defense=0, power=3),
+    inventory=Inventory(capacity=0),
 )
 rat = Actor(
     char="R",
     color=(0, 127, 0),
     name="Rat",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=16, defense=1, power=4),
+    fighter=Fighter(hp=2, defense=1, power=4),
+    inventory=Inventory(capacity=0)
+)
+
+health_stim = Item(
+    char="!",
+    color=(127, 0, 255),
+    name="Health Stim",
+    consumable=consumable.HealingConsumable(amount=2)
+)
+battery_pack = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Battery Pack",
+    consumable=consumable.BatteryDamageConsumable(damage=1, maximum_range=2),
 )
